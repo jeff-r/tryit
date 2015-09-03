baf292824dcf3d64fb2b6a97f51278b1f07f6fe1
Jeff Roush <jeff@jeffroush.com>
2015-09-02 21:24:48 -0700

    Use markdown in note text

--- www/tryit/app/helpers/application_helper.rb
+++ www/tryit/app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
+require "redcarpet"
+
 module ApplicationHelper
+  def format_markdown(string)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, extensions = {})
+    markdown.render(string)
+  end
 end
